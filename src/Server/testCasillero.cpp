4142b762a6d5cdd46959059145d20b192cd41b16
Belovic94 <victor.belosevich94@gmail.com>
2020-06-16 14:35:18 -0300

    Uncomment server test

--- src/Server/testCasillero.cpp
+++ src/Server/testCasillero.cpp
@@ -39,53 +39,53 @@ void test01CasilleroVacio(){
 	else std::cout << "MAL 01" << '\n';
 }
 
-//void test02CasilleroNoVacio(){
-//	Casillero casillero;
-//	ObjetoJuego objeto;
-//	//casillero.agregarObjeto(std::move(objeto));
-//	casillero.agregarObjeto(&objeto);
-//
-//
-//	if(!casillero.estaVacio()){
-//		std::cout << "OK 02" << '\n';
-//	}
-//	else std::cout << "MAL 02" << '\n';
-//}
+void test02CasilleroNoVacio(){
+	Casillero casillero;
+	ObjetoJuego objeto;
+	//casillero.agregarObjeto(std::move(objeto));
+	casillero.agregarObjeto(&objeto);
 
-//void test03ObtenerObjetoDelCasillero(){
-//	Casillero casillero;
-//	ObjetoJuego objeto;
-//	//casillero.agregarObjeto(std::move(objeto));
-//	casillero.agregarObjeto(&objeto);
-//
-//	if(&objeto == casillero.obtenerObjeto()){
-//		std::cout << "OK 03" << '\n';
-//	}
-//	else {
-//		std::cout << "MAL 03" << '\n';
-//	}
-//}
-//
-//void test04EliminarObjetoDeCasillero(){
-//	Casillero casillero;
-//	ObjetoJuego objeto;
-//
-//	casillero.agregarObjeto(&objeto);
-//	ObjetoJuego* objetoEliminado = casillero.eliminarObjeto();
-//
-//
-//	if(&objeto == objetoEliminado){
-//		std::cout << "OK 04" << '\n';
-//	}
-//	else {
-//		std::cout << "MAL 04" << '\n';
-//	}
-//
-//	if(casillero.estaVacio()){
-//		std::cout << "OK 04" << '\n';
-//	}
-//	else std::cout << "MAL 04" << '\n';
-//}
+
+	if(!casillero.estaVacio()){
+		std::cout << "OK 02" << '\n';
+	}
+	else std::cout << "MAL 02" << '\n';
+}
+
+void test03ObtenerObjetoDelCasillero(){
+	Casillero casillero;
+	ObjetoJuego objeto;
+	//casillero.agregarObjeto(std::move(objeto));
+	casillero.agregarObjeto(&objeto);
+
+	if(&objeto == casillero.obtenerObjeto()){
+		std::cout << "OK 03" << '\n';
+	}
+	else {
+		std::cout << "MAL 03" << '\n';
+	}
+}
+
+void test04EliminarObjetoDeCasillero(){
+	Casillero casillero;
+	ObjetoJuego objeto;
+
+	casillero.agregarObjeto(&objeto);
+	ObjetoJuego* objetoEliminado = casillero.eliminarObjeto();
+
+
+	if(&objeto == objetoEliminado){
+		std::cout << "OK 04" << '\n';
+	}
+	else {
+		std::cout << "MAL 04" << '\n';
+	}
+
+	if(casillero.estaVacio()){
+		std::cout << "OK 04" << '\n';
+	}
+	else std::cout << "MAL 04" << '\n';
+}
 
 void test05ObtenerObjetoCasilleroVacio(){
 	try{
